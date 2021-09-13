b503a4441c8477ed4f4c0e3f4fa8fd702c093e01
土田健斗 <tsuchitakento@tsuchitakentonoMacBook-Pro.local>
2021-09-14 01:37:37 +0900

    一時保存版

--- src/views/Top.vue
+++ src/views/Top.vue
@@ -4,17 +4,10 @@
     <main>
       <div class="relative">
         <video class="hidden md:block" :src="mainVideo" autoplay muted></video>
-        <video
-          class="md:hidden pt-20 w-full"
-          :src="mainVideoSp"
-          autoplay
-          muted
-        ></video>
+        <video class="md:hidden pt-20 w-full" :src="mainVideoSp" autoplay muted></video>
       </div>
       <div class="md:px-24 pl-20">
-        <h2 class="catch text-4xl text-left pt-20 md:pb-10 pb-5 md:pr-0 pr-2">
-          WORK
-        </h2>
+        <h2 class="catch text-4xl text-left pt-20 md:pb-10 pb-5 md:pr-0 pr-2">WORKS</h2>
         <div>
           <div class="md:grid md:grid-cols-4 md:grid-lows-2 gap-2 md:pr-0 pr-2">
             <a
@@ -60,21 +53,15 @@
         </div>
       </div>
       <div class="md:px-24 pl-20 pb-40">
-        <h2
-          class="catch text-4xl text-left md:pt-32 md:pb-10 pt-20 pb-5 md:pr-0 pr-2"
-        >
+        <h2 class="catch text-4xl text-left md:pt-32 md:pb-10 pt-20 pb-5 md:pr-0 pr-2">
           PROFILE
         </h2>
         <div class="md:flex content-center items-center relative">
           <div class="md:w-2/4 md:mr-0 mr-2">
             <img :src="profileImg" alt="profile" />
           </div>
-          <div
-            class="flex justify-between flex-col md:w-2/4 text-center content-center"
-          >
-            <p class="md:text-4xl text-2xl md:text-center text-left">
-              Tomoaki Sato
-            </p>
+          <div class="flex justify-between flex-col md:w-2/4 text-center content-center">
+            <p class="md:text-4xl text-2xl md:text-center text-left">Tomoaki Sato</p>
             <router-link
               id="profile"
               to="/profile"
@@ -105,8 +92,7 @@ export default {
           id: 1,
           img: require("../assets/img/MakeYouHappy-Kevin.png"),
           href: "https://www.youtube.com/watch?v=siFt7m8S8OY",
-          title:
-            "【アメリカ人が歌う】Make you happy / NiziU ‐ Feat.ケビン【アカペラ】",
+          title: "【アメリカ人が歌う】Make you happy / NiziU ‐ Feat.ケビン【アカペラ】",
           class: "sm:col-span-2 sm:text-base text-xs",
           imgclass: "md:w-3/4",
         },
@@ -114,8 +100,7 @@ export default {
           id: 2,
           img: require("../assets/img/citrus-daice.png"),
           href: "https://www.youtube.com/watch?v=ojLVyEszPpM",
-          title:
-            "【コラボ】CITRUS / Da-iCE【ハイスクール・バンバンの女性陣とアカペラ】",
+          title: "【コラボ】CITRUS / Da-iCE【ハイスクール・バンバンの女性陣とアカペラ】",
           class: "md:col-span-2 md:text-base text-xs",
           imgclass: "md:w-3/4",
         },
@@ -130,24 +115,21 @@ export default {
           id: 4,
           img: require("../assets/img/MGA-medley.png"),
           href: "https://www.youtube.com/watch?v=jgDJv06fgnU",
-          title:
-            "【ハモネプ出場者が歌う】インフェルノから始まるMrs. GREEN APPLE…",
+          title: "【ハモネプ出場者が歌う】インフェルノから始まるMrs. GREEN APPLE…",
           class: "text-xs",
         },
         {
           id: 5,
           img: require("../assets/img/Kevin-taik1.jpg"),
           href: "https://www.youtube.com/watch?v=bhGjP32JyL8",
-          title:
-            "【10分で上手くなる！】ネイティブに英語の発音レッスンをお願い…",
+          title: "【10分で上手くなる！】ネイティブに英語の発音レッスンをお願い…",
           class: "text-xs",
         },
         {
           id: 6,
           img: require("../assets/img/cleaning.jpg"),
           href: "https://www.youtube.com/watch?v=KhudW2ePa10",
-          title:
-            "【エアコン】誰でもできる！掃除のプロが教えるエアコンクリーニング",
+          title: "【エアコン】誰でもできる！掃除のプロが教えるエアコンクリーニング",
           class: "text-xs",
         },
       ],
