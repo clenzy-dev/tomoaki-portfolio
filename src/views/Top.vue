6f82f5f71901d5d82f7f0651456cc0a275a15697
土田健斗 <tsuchitakento@tsuchitakentonoMacBook-Pro.local>
2021-09-25 23:42:37 +0900

    ピクチャインピクチャ解消

--- src/views/Top.vue
+++ src/views/Top.vue
@@ -3,13 +3,20 @@
     <HeaderItem />
     <main>
       <div class="relative">
-        <video class="hidden md:block" :src="mainVideo" autoplay muted></video>
+        <video
+          class="hidden md:block"
+          :src="mainVideo"
+          autoplay
+          muted
+          disablePictureInPicture
+        ></video>
         <video
           class="md:hidden pt-20 w-full"
           :src="mainVideoSp"
           autoplay
           muted
           playsinline
+          disablePictureInPicture
         ></video>
       </div>
       <div class="md:px-24 pl-24 pt-4">
